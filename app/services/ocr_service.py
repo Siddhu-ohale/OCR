a9b7496b1292a5d9d1534fa71597f0ecd185bfaf
Siddharth Ohale <siddharthohale4@gmail.com>
2025-06-06 14:06:28 +0530

    Update ocr_service.py

--- app/services/ocr_service.py
+++ app/services/ocr_service.py
@@ -8,3 +8,4 @@ def extract_raw_text(image_path):
     image = ImageEnhance.Contrast(image).enhance(2.0)
     image = image.resize((image.width * 2, image.height * 2))
     return pytesseract.image_to_string(image, config='--oem 3 --psm 6')
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
