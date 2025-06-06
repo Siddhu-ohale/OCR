75df7a3cad86013b5c248ee8e6e9902e326b76f9
Siddhu-ohale <siddharthohale4@gmail.com>
2025-06-06 12:38:35 +0530

    First commit

---
+++ app/services/ocr_service.py
@@ -0,0 +1,10 @@
+
+from PIL import Image, ImageEnhance, ImageFilter
+import pytesseract
+
+def extract_raw_text(image_path):
+    image = Image.open(image_path).convert("L")
+    image = image.filter(ImageFilter.SHARPEN)
+    image = ImageEnhance.Contrast(image).enhance(2.0)
+    image = image.resize((image.width * 2, image.height * 2))
+    return pytesseract.image_to_string(image, config='--oem 3 --psm 6')
